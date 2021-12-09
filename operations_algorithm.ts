baeaef024ef805378af982e3b616e87a589aec23
von2020 <orumwensey@gmail.com>
2021-12-09 10:05:05 +0100

    Number One Challenge

--- operations_algorithm.ts
+++ operations_algorithm.ts
@@ -2,15 +2,16 @@
 
 //THIS IS THE ALGORITHM
 /** Step 1: Start
-    Step 2: Create a function for generating numbers
-    Step 3: assign function to a variable
+    Step 2: Create a function for generating numbers 
+    Step 3: assign function to a variable called generateInt
     step 4: Create another function passing in string x, y and z as parameters
-    Step 5: Instantiate the function generating numbers in this function passing parameters x into it, then assign the function to a variable1
-    step 6: Instantiate the function generating numbers in this function passing parameters y into it, then assign the function to a variable2
-    step 7: If z = "-" , then return the value of variable1 - variable2
-    step 8: If z = "+" , then return the value of variable1 + variable2
-    step 9: If z = "*", then return the value of variable1 * variable2
-    step 10:If z = "/", then return the value of variable1 / variable2
+    step 5: assign function2 to a variable called calculateString
+    Step 6: Instantiate the function generating numbers in this function2 passing parameters x into it, then assign the function to a variable1
+    step 7: Instantiate the function generating numbers in this function2 passing parameters y into it, then assign the function to a variable2
+    step 8: If z = "-" , then return the value of variable1 - variable2
+    step 9: If z = "+" , then return the value of variable1 + variable2
+    step 10: If z = "*", then return the value of variable1 * variable2
+    step 11:If z = "/", then return the value of variable1 / variable2
 
     Step 6: Stop
 
@@ -31,7 +32,7 @@ let generateInt = function (x) {
    
 }
 
-let calculateNumbers = function (x, y, z) {
+let calculateString = function (x, y, z) {
     let numb1 = generateInt(x);
     let numb2 = generateInt(y);
     if (z == '-') return numb1 - numb2
@@ -40,7 +41,7 @@ let calculateNumbers = function (x, y, z) {
     else return numb1 * numb2;
 }
 
-console.log(calculateNumbers('2', '2', '+'));
-console.log(calculateNumbers('2', '2', '-'));
-console.log(calculateNumbers('2', '2', '*'));
-console.log(calculateNumbers('2', '2', '/'));
+console.log(calculateString('2', '2', '+'));
+console.log(calculateString('2', '2', '-'));
+console.log(calculateString('2', '2', '*'));
+console.log(calculateString('2', '2', '/'));
