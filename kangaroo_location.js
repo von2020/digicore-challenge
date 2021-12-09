d226dccbf43f34682f1751d43e518e0b983f8863
von2020 <orumwensey@gmail.com>
2021-12-09 10:27:59 +0100

    Number Three Challenge

---
+++ kangaroo_location.js
@@ -0,0 +1,16 @@
+function kangaroo(x1,x2,v1,v2){
+	var k1 = x1 * v1
+	var k2 = x2 * v2
+	var result1 = "YES";
+	var result2 = "NO";
+	if (k1 = k2){
+	  console.log(result1)
+    return result1;
+	} 
+	else{
+	  console.log(result2)
+	return result2;
+	}
+}
+
+kangaroo(2,3,3,2);
